66354d239850a9af5f4707c9952013b18cb6b4a4
Rachnerd <rachnerd@localhost.localdomain>
2016-06-26 18:51:36 +0200

    Added browser-sync

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 #React + Webpack
 ##Install
+_For more detail: [package.json dep + devDep](./package.json)_
 ```
 npm i
 ```
 ##Run
+_For more detail: [package.json scripts](./package.json)_
 ```
 npm run dev
 or
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,51 @@
+
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
 module.exports = {
-    entry: "./src/index.tsx",
+    entry: [
+        './src/index.tsx'
+    ],
+
     output: {
         filename: "./dist/bundle.js",
     },
 
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
 
     resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
         extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
     },
 
     module: {
         loaders: [
             // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
-            { test: /\.tsx?$/, loader: "ts-loader" }
+            { test: /\.tsx?$/, loader: "ts-loader" },
         ],
 
         preLoaders: [
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { test: /\.js$/, loader: "source-map-loader" }
+            { test: /\.js$/, loaders: ["source-map-loader", "react-hot"] }
         ]
     },
-
+    plugins: [
+        new CleanWebpackPlugin(['dist'], {
+            root: __dirname,
+            verbose: true,
+            dry: false
+        }),
+        new CopyWebpackPlugin([
+            { context: 'src', from: '**/*.*', to: './dist', ignore: ['*.tsx', '*.ts', '*.scss'] },
+            { context: 'node_modules', from: 'react/dist/react.min.js', to: './dist/libs'},
+            { context: 'node_modules', from: 'react-dom/dist/react-dom.min.js', to: './dist/libs'}
+        ]),
+        new BrowserSyncPlugin({
+            port: 3000,
+            server: 'dist',
+            ui: false
+        })
+    ],
     // When importing a module whose path matches one of the following, just
     // assume a corresponding global variable exists and use that instead.
     // This is important because it allows us to avoid bundling all of our
@@ -31,5 +53,5 @@ module.exports = {
     externals: {
         "react": "React",
         "react-dom": "ReactDOM"
-    },
+    }
 };
